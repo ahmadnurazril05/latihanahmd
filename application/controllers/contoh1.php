d04a8040479471d81eb68c66b37ce284f5a1dfbc
ahmadnurazril05 <azril081510958001@gmail.com>
2023-09-25 15:46:07 +0700

    penambahan ci

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,7 @@
+<?php
+class Contoh1 extends CI_Controller { public function index()
+     {
+    echo "<h1>Perkenalkan</h1>"; 
+    echo"Nama saya Ahmad Nur Azriel<br> Saya tingga di daerah Jelambar olah raga yang saya sukai adalah Basket";                 
+    } 
+}
